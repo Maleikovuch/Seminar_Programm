d81814aacfc59e8be982c77a9affa74cfc4883c6
«maleikovuch» <maleikovuch@mail.ru>
2022-09-15 23:46:28 +0400

    Добавили решение задачи 5

---
+++ Task05/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = -number;
+while(i <= number)
+{
+    Console.Write($" {i}, ");
+    i++;
+}
+
