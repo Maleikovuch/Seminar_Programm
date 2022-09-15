f7bafe6e3bc1122964c54c0e67dc9f82ef3ad369
«maleikovuch» <maleikovuch@mail.ru>
2022-09-16 00:53:01 +0400

    Добавили решение задачт 3 первым спосбом (команда свитч)

---
+++ Task03/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите целое число от 1 до 7 и увидете какой день недели!");
+int number = Convert.ToInt32(Console.ReadLine());
+switch (number)
+{
+    case 1: Console.WriteLine("Понедельник"); break;
+    case 2: Console.WriteLine("Вторник"); break;
+    case 3: Console.WriteLine("Среда"); break;
+    case 4: Console.WriteLine("Четверг"); break;
+    case 5: Console.WriteLine("Пятница"); break;
+    case 6: Console.WriteLine("Суббота"); break;
+    case 7: Console.WriteLine("Воскресенье"); break;
+    default: Console.WriteLine("В неделе 7 дней"); break;
+}
