cdcc212a2347abf832f2e6df448711a2b13f5fe3
«maleikovuch» <maleikovuch@mail.ru>
2022-09-19 00:14:32 +0400

    Добавили решение задачи 6

---
+++ Task06/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите целое число и узнаете является ли оно четным или не четным: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число не четное");
+}
