7423a906042e3445f1e51c404ca5707e34c295ec
«maleikovuch» <maleikovuch@mail.ru>
2022-09-18 22:53:15 +0400

    Добавили решение задачи 2

---
+++ Task02/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите два целых числа и вы увидете какое число меньшее, а какое число большее! ");
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+int min = b;
+if (b > max)
+{
+    Console.WriteLine($"Большее число - {b}");
+    Console.WriteLine($"Меньшее число - {a}");
+}
+else
+{
+    Console.WriteLine($"Большее число - {a}");
+    Console.WriteLine($"Меньшее число - {b}");
+}
+    
