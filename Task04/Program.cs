3f1fd096867fff241404749094a683fbb88fd3b0
«maleikovuch» <maleikovuch@mail.ru>
2022-09-19 21:05:26 +0400

    Убрал лишнее условие в задаче 4

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -7,7 +7,6 @@ Console.WriteLine("Введите третье число: ");
 int c = Convert.ToInt32(Console.ReadLine());
 int max = a;
 
-if (a > max) max = a;
 if (b > max) max = b;
 if (c > max) max = c;
 Console.Write($"Максимальное число - {max} ");
