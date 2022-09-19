6f9ed24a050515470b8996dad1d4b1b4dceab059
«maleikovuch» <maleikovuch@mail.ru>
2022-09-19 21:37:44 +0400

    Добавили задачу 9

---
+++ Task09/Program.cs
@@ -0,0 +1,14 @@
+﻿int number = new Random().Next(10, 100);
+System.Console.WriteLine($"Случайное число из отрезка 10-99 => {number}");
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+if(firstDigit == secondDigit) System.Console.WriteLine("Цифры одинаковые");
+if(firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа - {number} => {firstDigit}");
+else Console.WriteLine($"Наибольшая цифра числа - {number} => {secondDigit}");
+
+int maxDigit = 0;
+if(firstDigit > secondDigit) maxDigit = firstDigit;
+else maxDigit = secondDigit;
+
+int max = firstDigit > secondDigit ? firstDigit : secondDigit;
