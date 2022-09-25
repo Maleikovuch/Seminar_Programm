4d6f40f57e5a23abdac1d06e5c20354b0093e89e
«maleikovuch» <maleikovuch@mail.ru>
2022-09-25 22:54:27 +0400

    Добавил задачи из семинара

---
+++ Task20/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите координаты первой точки: ");
+Console.Write("Х: ");
+int argAX = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int argAY = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки: ");
+Console.Write("X: ");
+int argBX = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int argBY = Convert.ToInt32(Console.ReadLine());
+
+// int argX = argBX - argAX;
+// int argY = argBY - argAY;
+
+// double argZ = Math.Sqrt(argX*argX + argY*argY);
+
+// Console.WriteLine($"-> {argZ}");
+
+double LenghLine(int ax, int ay, int bx, int by)
+{
+    int x = bx - ax;
+    int y = by - ay;
+    return Math.Sqrt(x * x + y * y);
+}
+double lenghLine = LenghLine(argAX, argAY, argBX, argBY);
+double lenght = Math.Round(lenghLine, 2, MidpointRounding.ToZero);
+Console.WriteLine($"A({argAX}, {argAY}); B ({argBX}, {argBY})->{lenght}");
---
+++ Task22/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+void Square(int num)
+{
+    int count = 1;
+    if (count > 0)
+    {
+        while (count <= a)
+        {
+            Console.WriteLine($"{count}   {Math.Pow((count), 2),3}");
+            count++;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Ввели не корректное значение");
+    }
+}
+Square(a);
+
+
