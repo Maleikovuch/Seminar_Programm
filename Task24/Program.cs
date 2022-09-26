72e369a966048732ec0af0fc8019cd2c47e24fe8
«maleikovuch» <maleikovuch@mail.ru>
2022-09-26 23:40:15 +0400

    Добавил задачи из семинара

---
+++ Task24/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sumnumbers = SumNumbers2(number);
+Console.WriteLine($"Сумма чисел от 1 до {number}= {sumnumbers}");
+
+int SumNumbers(int num)
+{
+    int sum = default;
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+int SumNumbers2(int num)
+{
+    int sum = default;
+    int i = 1;
+    while (i <= num)
+    {
+        sum = sum + i;
+        i++;
+    }
+    return sum;
+}
---
+++ Task30/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int[] arrayGlobal = new int[8];
+FillArray(arrayGlobal);
+PrintArray(arrayGlobal);
+
+void FillArray(int[] array)
+
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+        array[i] = rnd.Next(0, 2);
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length-1; i++) 
+    Console.Write($"{array[i]},");
+    Console.Write($"{array[array.Length-1]}]");
+}
