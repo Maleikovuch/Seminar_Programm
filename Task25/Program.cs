dee18861260f7e66eefa15cc37c549302c4f81bb
«maleikovuch» <maleikovuch@mail.ru>
2022-10-02 17:15:28 +0400

    Добавил домашнее задание с семинара 4

---
+++ Task25/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.Write("Введите два числа: ");
+Console.Write("Введите первое число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+void Exponent(int num, int deg)
+{
+    int result = 1;
+    if (deg > 0)
+    {
+        for (int i = 0; i < deg; i++)
+        {
+            result = result * num;
+        }
+        Console.WriteLine(result);
+    }
+    else
+        Console.WriteLine("Вы ввели не корректное значение");
+}
+Exponent(A, B);
+
+
---
+++ Task29/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] arrayGlobal = new int[8];
+FillArray(arrayGlobal);
+PrintArray(arrayGlobal);
+
+void FillArray(int[] array)
+
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+        array[i] = rnd.Next(0, 100);
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length-1; i++) 
+    Console.Write($"{array[i]},");
+    Console.Write($"{array[array.Length-1]}]");
+}
