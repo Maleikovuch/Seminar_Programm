8b0da0277b34619d13bffa2ec483f6735cfc78fd
«maleikovuch» <maleikovuch@mail.ru>
2022-10-02 17:24:15 +0400

    Правки в 27 задаче

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -6,32 +6,32 @@
 
 // 9012 -> 12
 
-// Console.WriteLine("Введите число: ");
-// int number = Convert.ToInt32(Console.ReadLine());
-// int SumNumbers2(int num)
-// {
-//     int sum = 0;
-//     for (int i = 0; i < num; i++)
-//     {
-//         sum = sum + (num % 10);
-//         num = num / 10;
-//     }
-//     return sum;
-// }
-// int sumnumbers = SumNumbers2(number);
-// Console.WriteLine($"Сумма цифр в числе {number} = {sumnumbers}");
-
 Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int SumNumbers2(int num)
+int SumNumbers(int num)
 {
     int sum = 0;
-    while (num > 0)
+    for (int i = 0; i < num; i++)
     {
         sum = sum + (num % 10);
         num = num / 10;
     }
     return sum;
 }
-int sumnumbers = SumNumbers2(number);
+int sumnumbers = SumNumbers(number);
 Console.WriteLine($"Сумма цифр в числе {number} = {sumnumbers}");
+
+// Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int SumNumbers(int num)
+// {
+//     int sum = 0;
+//     while (num > 0)
+//     {
+//         sum = sum + (num % 10);
+//         num = num / 10;
+//     }
+//     return sum;
+// }
+// int sumnumbers = SumNumbers(number);
+// Console.WriteLine($"Сумма цифр в числе {number} = {sumnumbers}");
